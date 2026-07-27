dbca0c48815c253c8040f15dbe0c5b9952319ca4
sgnmeta7 <sgnmeta@gmail.com>
2026-07-27 23:50:10 +0530

    Update API endpoint to production URL

--- front/src/UI.jsx
+++ front/src/UI.jsx
@@ -84,7 +84,7 @@ export default function UI() {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://anime-rating-predictor.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(input),
