db06ec95a020d22d39b801aadfb32c7fdbf99cf5
sgnmeta7 <sgnmeta@gmail.com>
2026-07-28 00:32:40 +0530

    updated cors line to allow reqs from vercel frontend

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 model = joblib.load('xgb_model.pkl')
 columns = joblib.load('model_columns.pkl')
